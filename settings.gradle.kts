680a270b8fd50761d6ff0a79ca3cbeb5d0cad661
Harsh Shandilya <me@msfjarvis.dev>
2022-01-01 22:29:17 +0530

    gradle: upgrade Kotlin and Spotless

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,14 +7,14 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("VERSION_CATALOGS")
 
 pluginManagement {
-  val kotlinVersion = "1.5.21"
+  val kotlinVersion = "1.6.10"
   repositories {
     gradlePluginPortal()
     mavenCentral()
   }
   plugins {
     id("org.jetbrains.kotlin.jvm") version "$kotlinVersion"
-    id("com.diffplug.spotless") version "5.14.3"
+    id("com.diffplug.spotless") version "6.1.0"
   }
 }
 
