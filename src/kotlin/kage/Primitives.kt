95bc9200925cd0ff6a11c3d697ec9b5ca56935d4
Aditya <adityawasan55@gmail.com>
2022-01-01 21:08:13 +0530

    feat(core): add `streamKey` method
    Signed-off-by: Aditya <adityawasan55@gmail.com>

--- src/kotlin/kage/Primitives.kt
+++ src/kotlin/kage/Primitives.kt
@@ -2,24 +2,27 @@ package kage
 
 import at.favre.lib.crypto.HKDF
 import kage.format.AgeHeader
-import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
 internal object Primitives {
+
+  private const val MAC_ALGORITHM = "HmacSHA256"
+  private const val MAC_KEY_LENGTH = 32
+  private const val CHACHA_20_POLY_1305_KEY_LENGTH = 32
+
   fun headerMAC(fileKey: ByteArray, header: AgeHeader): ByteArray {
-    val hmacAlgorithm = "HmacSHA256"
-    val headerByteArray = "header".encodeToByteArray()
-    val hkdf = HKDF.fromHmacSha256()
     // Passing null directly in extractAndExpand causes overload ambiguity since both SecretKey and ByteArray can be null
     // so create a null variable of type ByteArray
     val saltExtract: ByteArray? = null
+    val headerByteArray = "header".encodeToByteArray()
+    val hkdf = HKDF.fromHmacSha256()
 
-    val hmacKey = hkdf.extractAndExpand(saltExtract, fileKey, headerByteArray, 32)
-    val secretKey = SecretKeySpec(hmacKey, hmacAlgorithm)
+    val hmacKey = hkdf.extractAndExpand(saltExtract, fileKey, headerByteArray, MAC_KEY_LENGTH)
+    val secretKey = SecretKeySpec(hmacKey, MAC_ALGORITHM)
 
-    val mac = Mac.getInstance(hmacAlgorithm)
+    val mac = Mac.getInstance(MAC_ALGORITHM)
     mac.init(secretKey)
 
     val outputStream = ByteArrayOutputStream()
@@ -29,5 +32,11 @@ internal object Primitives {
 
     // Here we need to pass the complete header including the footer prefix
     return mac.doFinal(outputStream.toByteArray())
+  }
+
+  fun streamKey(fileKey: ByteArray, nonce: ByteArray): ByteArray {
+    val payloadByteArray = "payload".encodeToByteArray()
+    val hkdf = HKDF.fromHmacSha256()
+    return hkdf.extractAndExpand(nonce, fileKey, payloadByteArray, CHACHA_20_POLY_1305_KEY_LENGTH)
   }
 }
