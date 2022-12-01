3b3e52b6b8848b02075a273579851aeb0455ed49
Harsh Shandilya <me@msfjarvis.dev>
2022-12-01 15:24:54 +0530

    feat(test): add types for upstream test suite

---
+++ src/test/kotlin/kage/test/utils/types.kt
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2021 The kage Authors. All rights reserved. Use of this source code is governed by
+ * either an Apache 2.0 or MIT license at your discretion, that can be found in the LICENSE-APACHE
+ * or LICENSE-MIT files respectively.
+ */
+package kage.test.utils
+
+enum class Expect {
+  Success,
+  HMACFailure,
+  HeaderFailure,
+  ArmorFailure,
+  PayloadFailure,
+  NoMatch,
+  ;
+  companion object {
+    fun fromString(value: String): Expect {
+      return when (value) {
+        "success" -> Success
+        "HMAC failure" -> HMACFailure
+        "header failure" -> HeaderFailure
+        "armor failure" -> ArmorFailure
+        "payload failure" -> PayloadFailure
+        "no match" -> NoMatch
+        else -> throw IllegalArgumentException("unknown expect value: $value")
+      }
+    }
+  }
+}
+
+class PayloadHash private constructor(val bytes: ByteArray) {
+  companion object {
+    fun from(value: String): PayloadHash {
+      check(value.length % 2 == 0) { "Must have an even length" }
+      val hex = value.chunked(2).map { it.toInt(16).toByte() }.toByteArray()
+      check(hex.size == 32) { "Payload hash should be exactly 32 bytes" }
+      return PayloadHash(hex)
+    }
+  }
+}
