3ad6f94eae08454918eca4b767cfbd13a7a7251b
Harsh Shandilya <me@msfjarvis.dev>
2025-08-21 23:33:23 +0530

    fixup! chore(deps): update dependency com.facebook:ktfmt to v0.57

--- src/test/kotlin/kage/format/AgeHeaderTest.kt
+++ src/test/kotlin/kage/format/AgeHeaderTest.kt
@@ -19,13 +19,14 @@ class AgeHeaderTest {
   @Test
   fun testCompleteHeader() {
     val headerString =
-      """age-encryption.org/v1
-            |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
-            |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
-            |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
-            |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
-            |--- gxhoSa5BciRDt8lOpYNcx4EYtKpS0CJ06F3ZwN82VaM
-            |"""
+      """
+      |age-encryption.org/v1
+      |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
+      |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
+      |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
+      |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
+      |--- gxhoSa5BciRDt8lOpYNcx4EYtKpS0CJ06F3ZwN82VaM
+      |"""
         .trimMargin()
 
     val reader = headerString.byteInputStream().buffered()
@@ -77,13 +78,14 @@ class AgeHeaderTest {
   @Test
   fun testEquality() {
     val headerString =
-      """age-encryption.org/v1
-            |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
-            |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
-            |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
-            |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
-            |--- gxhoSa5BciRDt8lOpYNcx4EYtKpS0CJ06F3ZwN82VaM
-            |"""
+      """
+      |age-encryption.org/v1
+      |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
+      |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
+      |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
+      |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
+      |--- gxhoSa5BciRDt8lOpYNcx4EYtKpS0CJ06F3ZwN82VaM
+      |"""
         .trimMargin()
     val header1 = AgeHeader.parse(headerString.byteInputStream().buffered())
     val header2 = AgeHeader.parse(headerString.byteInputStream().buffered())
@@ -94,25 +96,26 @@ class AgeHeaderTest {
   @Test
   fun testMultipleRecipients() {
     val recipients =
-      """-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
-            |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
-            |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
-            |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
-            |-> scrypt GixTkc7+InSPLzPNGU6cFw 18
-            |kC4zjzi7LRutdBfOlGHCgox8SXgfYxRYhWM1qPs0ca8
-            |-> ssh-rsa SkdmSg
-            |SW+xNSybDWTCkWx20FnCcxlfGC889s2hRxT8+giPH2DQMMFV6DyZpveqXtNwI3ts
-            |5rVkW/7hCBSqEPQwabC6O5ls75uNjeSURwHAaIwtQ6riL9arjVpHMl8O7GWSRnx3
-            |NltQt08ZpBAUkBqq5JKAr20t46ZinEIsD1LsDa2EnJrn0t8Truo2beGwZGkwkE2Y
-            |j8mC2GaqR0gUcpGwIk6QZMxOdxNSOO7jhIC32nt1w2Ep1ftk9wV1sFyQo+YYrzOx
-            |yCDdUwQAu9oM3Ez6AWkmFyG6AvKIny8I4xgJcBt1DEYZcD5PIAt51nRJQcs2/ANP
-            |+Y1rKeTsskMHnlRpOnMlXqoeN6A3xS+EWxFTyg1GREQeaVztuhaL6DVBB22sLskw
-            |XBHq/XlkLWkqoLrQtNOPvLoDO80TKUORVsP1y7OyUPHqUumxj9Mn/QtsZjNCPyKN
-            |ds7P2OLD/Jxq1o1ckzG3uzv8Vb6sqYUPmRvlXyD7/s/FURA1GetBiQEdRM34xbrB
-            |
-            |-> ssh-ed25519 Xyg06A rH24zuz7XHFc1lRyQmMrekpLrcKrJupohEh/YjvQCxs
-            |Bbtnl6veSZhZmG7uXGQUX0hJbrC8mxDkL3zW06tqlWY
-            |---"""
+      """
+      |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
+      |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
+      |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
+      |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
+      |-> scrypt GixTkc7+InSPLzPNGU6cFw 18
+      |kC4zjzi7LRutdBfOlGHCgox8SXgfYxRYhWM1qPs0ca8
+      |-> ssh-rsa SkdmSg
+      |SW+xNSybDWTCkWx20FnCcxlfGC889s2hRxT8+giPH2DQMMFV6DyZpveqXtNwI3ts
+      |5rVkW/7hCBSqEPQwabC6O5ls75uNjeSURwHAaIwtQ6riL9arjVpHMl8O7GWSRnx3
+      |NltQt08ZpBAUkBqq5JKAr20t46ZinEIsD1LsDa2EnJrn0t8Truo2beGwZGkwkE2Y
+      |j8mC2GaqR0gUcpGwIk6QZMxOdxNSOO7jhIC32nt1w2Ep1ftk9wV1sFyQo+YYrzOx
+      |yCDdUwQAu9oM3Ez6AWkmFyG6AvKIny8I4xgJcBt1DEYZcD5PIAt51nRJQcs2/ANP
+      |+Y1rKeTsskMHnlRpOnMlXqoeN6A3xS+EWxFTyg1GREQeaVztuhaL6DVBB22sLskw
+      |XBHq/XlkLWkqoLrQtNOPvLoDO80TKUORVsP1y7OyUPHqUumxj9Mn/QtsZjNCPyKN
+      |ds7P2OLD/Jxq1o1ckzG3uzv8Vb6sqYUPmRvlXyD7/s/FURA1GetBiQEdRM34xbrB
+      |
+      |-> ssh-ed25519 Xyg06A rH24zuz7XHFc1lRyQmMrekpLrcKrJupohEh/YjvQCxs
+      |Bbtnl6veSZhZmG7uXGQUX0hJbrC8mxDkL3zW06tqlWY
+      |---"""
         .trimMargin()
 
     val reader = recipients.byteInputStream().buffered()
@@ -124,11 +127,12 @@ class AgeHeaderTest {
   @Test
   fun testReaderPositionAfterParsingRecipients() {
     val recipients =
-      """-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
-            |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
-            |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
-            |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
-            |---"""
+      """
+      |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
+      |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
+      |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
+      |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
+      |---"""
         .trimMargin()
 
     val reader = recipients.byteInputStream().buffered()
@@ -144,13 +148,14 @@ class AgeHeaderTest {
   @Test
   fun testWriteAgeHeaderWithoutMac() {
     val header =
-      """age-encryption.org/v1
-            |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
-            |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
-            |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
-            |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
-            |--- gxhoSa5BciRDt8lOpYNcx4EYtKpS0CJ06F3ZwN82VaM
-            |"""
+      """
+      |age-encryption.org/v1
+      |-> X25519 SVrzdFfkPxf0LPHOUGB1gNb9E5Vr8EUDa9kxk04iQ0o
+      |0OrTkKHpE7klNLd0k+9Uam5hkQkzMxaqKcIPRIO1sNE
+      |-> X25519 8hWaIUmk67IuRZ41zMk2V9f/w3f5qUnXLL7MGPA+zE8
+      |tXgpAxKgqyu1jl9I/ATwFgV42ZbNgeAlvCTJ0WgvfEo
+      |--- gxhoSa5BciRDt8lOpYNcx4EYtKpS0CJ06F3ZwN82VaM
+      |"""
         .trimMargin()
 
     val reader = header.byteInputStream().buffered()
@@ -167,8 +172,7 @@ class AgeHeaderTest {
   fun testEmptyReader() {
     val header =
       """
-      |age-encryption.org/v1
-    """
+      |age-encryption.org/v1"""
         .trimMargin()
 
     assertThrows<InvalidRecipientException> { AgeHeader.parse(header.byteInputStream().buffered()) }
